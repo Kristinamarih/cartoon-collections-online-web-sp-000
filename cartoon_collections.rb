ad58c3563efe6830920d8cd3241db15db864a5b9
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-03-27 04:04:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.include?()
+  planeteer_calls.each do |call|
+  planeteer_calls.include?(call.length > 4)
     
 end
 
