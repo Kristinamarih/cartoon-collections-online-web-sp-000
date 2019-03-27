d45c1587876e1b2e90042c3d2a3dd7ce54f418ce
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-03-27 04:15:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,11 +7,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.each do |call|
   planeteer_calls.any?{|call| call.length > 4}
-    
-    end
-  end
 end
 
 def find_the_cheese# code an argument here
